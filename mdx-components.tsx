a530b8bfce6be4b334dec419378f6cb99d90c212
antho1404 <anthony.estebe@gmail.com>
2025-05-12 17:54:49 +0700

    add initial base

---
+++ mdx-components.tsx
@@ -0,0 +1,38 @@
+import type { MDXComponents } from "mdx/types";
+import Image from "next/image";
+import Link from "next/link";
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    a: ({ href, children, ...props }) => {
+      const className = "underline underline-offset-4 text-primary";
+      if (href?.startsWith("/")) {
+        return (
+          <Link href={href} className={className} {...props}>
+            {children}
+          </Link>
+        );
+      }
+      if (href?.startsWith("#")) {
+        return (
+          <a href={href} className={className} {...props}>
+            {children}
+          </a>
+        );
+      }
+      return (
+        <a
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={className}
+          {...props}
+        >
+          {children}
+        </a>
+      );
+    },
+    img: (props) => <Image {...props} />,
+    ...components,
+  };
+}
