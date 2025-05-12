7afd825a0d6af884bbb22c00d3f9acabbfe356e8
antho1404 <anthony.estebe@gmail.com>
2025-05-12 23:52:56 +0700

    remove analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -61,7 +60,6 @@ export default function RootLayout({
             </div>
           </footer>
         </div>
-        <Analytics />
       </body>
     </html>
   );
